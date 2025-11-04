dc6566d3c2178125c902439f7c6e0fc6ba804617
EeLin02 <p23015080@student.newinti.edu.my>
2025-11-04 20:51:41 +0800

    Initial commit - cleaned history

---
+++ client/src/Context.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import Cookies from 'js-cookie';
+import Data from './Data';
+
+const Context = React.createContext();
+
+export class Provider extends Component {
+  state = {
+    // FIXED: getJSON() removed in js-cookie v3, use get() + JSON.parse()
+    authenticatedUser: JSON.parse(Cookies.get('authenticatedUser') || 'null'),
+  };
+
+  constructor() {
+    super();
+    this.data = new Data();
+  }
+
+  render() {
+    const { authenticatedUser } = this.state;
+    const value = {
+      authenticatedUser,
+      data: this.data,
+      actions: {
+        signIn: this.signIn,
+        signOut: this.signOut,
+      },
+    };
+    return (
+      <Context.Provider value={value}>
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
+
+  /**
+   * Signs the user in by retrieving the user's details, setting the authenticatedUser state and browser cookies
+   * @param {String} emailAddress 
+   * @param {String} password 
+   * @returns {Object} user
+   */
+  signIn = async (emailAddress, password) => {
+    const user = await this.data.getUser(emailAddress, password);
+    if (user !== null && user.id) {
+      const authenticatedUser = { ...user, password };
+      this.setState({ authenticatedUser });
+      //  FIXED: use set() directly with stringified data
+      Cookies.set('authenticatedUser', JSON.stringify(authenticatedUser), { expires: 1 });
+    }
+    return user;
+  }
+
+  /**
+   * Signs the user out by setting a null authenticated user and removing cookies
+   */
+  signOut = () => {
+    this.setState({ authenticatedUser: null });
+    Cookies.remove('authenticatedUser');
+  }
+}
+
+export const Consumer = Context.Consumer;
+
+/**
+ * A higher-order component that wraps the provided component in a Context Consumer component.
+ * @param {class} Component - A React component.
+ * @returns {function} A higher-order component.
+ */
+export function withContext(Component) {
+  return function ContextComponent(props) {
+    return (
+      <Context.Consumer>
+        {context => <Component {...props} context={context} />}
+      </Context.Consumer>
+    );
+  }
+}
+
+const contextObjects = { withContext, Context };
+export default contextObjects;
